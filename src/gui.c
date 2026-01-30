9101828d78eb1a4b3995363685f036c82fbd51d8
drmcbrayer89 <drmcbrayer89@gmail.com>
2026-01-29 22:10:36 -0500

    Spawning debug window

--- src/gui.c
+++ src/gui.c
@@ -5,16 +5,26 @@
 
 SDL_Window * p_game_window;
 SDL_Renderer * p_renderer;
+SDL_Renderer * p_debug_renderer;
 SDL_Surface * p_surface;
 SDL_Window * p_debug_window;
 
-static void guiInitDebugWindow(void) {
+static bool guiInitDebugWindow(void) {
     if(SDL_Init(SDL_INIT_VIDEO)) {
         printf("SDL Debug Window Init Successful\n");
     }
     if(TTF_Init()) {
         printf("TTF Init Successful\n");
     }
+    
+    p_debug_window = SDL_CreateWindow("CGBEmu Debug", 640, 480, SDL_WINDOW_OPENGL);
+    
+    uint32_t x, y;
+    SDL_GetWindowPosition(p_game_window, &x, &y);
+    SDL_SetWindowPosition(p_debug_window, x + 650, y);
+    p_debug_renderer = SDL_CreateRenderer(p_debug_window, NULL);
+        
+    SDL_SetRenderDrawColor(p_debug_renderer, 0, 0, 0, 255);
 }
 
 static bool guiInitWindow(void) {
@@ -38,6 +48,7 @@ void guiRun() {
 
     while(SDL_PollEvent(&e) > 0){
         SDL_RenderPresent(p_renderer);
+        SDL_RenderPresent(p_debug_renderer);
 
         if(e.type == SDL_EVENT_KEY_DOWN && e.key.key == SDLK_ESCAPE){
             gbStopEmu();
@@ -46,5 +57,12 @@ void guiRun() {
 }
 
 bool guiInit(void) {
-    return guiInitWindow();
+    if(!guiInitWindow()) {
+        return false;
+    }
+    if(!guiInitDebugWindow()) {
+        return false;
+    }
+
+    return true;
 }
